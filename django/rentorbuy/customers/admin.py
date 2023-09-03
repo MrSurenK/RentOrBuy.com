6da6b246c210d5ff5ba7c2dc07e00fd285a1f760
K.Surendraraj <surenkworkz@gmail.com>
2023-09-03 16:15:22 +0800

    set up admin page with customer table

--- django/rentorbuy/customers/admin.py
+++ django/rentorbuy/customers/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import CustomerAccount
 
 # Register your models here.
+
+
+class CustomerAccountAdmin(admin.ModelAdmin):
+    exclude = ('last_login', 'valid_license', 'is_active')
+    fieldsets = (
+        ('Mandatory Information', {'fields': ('email', 'password', 'first_name', 'last_name', 'nric', 'age', 'contact_no', 'address',)}),
+        ('Additional Info', {'fields': ('profile_pic',)}),
+    )
+
+
+admin.site.register(CustomerAccount, CustomerAccountAdmin)
+
