a6697484b123e499d671adc477612d6c18acf88a
K.Surendraraj <surenkworkz@gmail.com>
2023-09-05 16:05:54 +0800

    Created serializers in dealer app

---
+++ django/rentorbuy/dealer/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Car, RentalListing, SaleListing
+
+class RentalListingSerializer(serializers, ModelSerializer):
+    class Meta:
+        model = RentalListing
+        fields = '__all__'
+
+class CarSerializer(serializers, ModelSerializer):
+    class Meta:
+        model = Car
+        fields = '__all__'
+
+class SaleListingSerializer(serializers, ModelSerializer):
+    class Meta:
+        model = SaleListing
+        fields =
+
+
+
+
+
+
+
+
