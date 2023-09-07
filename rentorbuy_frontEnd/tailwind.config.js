9b78eb1a7d267d67cb5950ab66e4d8a9b0138c0e
K.Surendraraj <surenkworkz@gmail.com>
2023-09-07 13:28:48 +0800

    Set up colours and font Inter

--- rentorbuy_frontEnd/tailwind.config.js
+++ rentorbuy_frontEnd/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["index.html", "./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#118a7e",
+        secondary: "#3baea0",
+        buttonMain: "#1f6f78",
+        buttonSecond: "#93e4c1",
+        neutralSilver: "#F5F7FA",
+        neutralGrey: "#4D4D4D",
+      },
+    },
   },
   plugins: [],
 };
