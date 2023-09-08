e7515901a24003ecf7e4846798c0a97b4216d180
MrSurenK <surenkworkz@gmail.com>
2023-09-08 15:12:22 +0800

    installed Flowbite-React

--- rentorbuy_frontEnd/src/components/Home.jsx
+++ rentorbuy_frontEnd/src/components/Home.jsx
@@ -5,8 +5,10 @@ const Home = () => {
   return (
     <>
       <div className="mt-clearNav bg-neutralSilver ">
-        <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen">
-          <img src={BannerCar} atl="Banner Car" />
+        <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen  ">
+          <div className="w-full mx-auto">
+            <img src={BannerCar} alt="Banner Car" />
+          </div>
         </div>
       </div>
     </>
