bbaba7356f4ede29f4575813d11692edbe1bb3b4
K.Surendraraj <surenkworkz@gmail.com>
2023-09-13 00:35:09 +0800

    date picker settled

--- rentorbuy_frontEnd/src/components/Home.jsx
+++ rentorbuy_frontEnd/src/components/Home.jsx
@@ -4,6 +4,7 @@ import BannerCar from "../assets/car_no_background.png";
 import { Card } from "flowbite-react";
 import Modal from "./Modal";
 import UserContext from "../context/user";
+import Datepicker from "react-tailwindcss-datepicker";
 
 const Home = () => {
   const fetchData = useFetch();
@@ -12,6 +13,16 @@ const Home = () => {
 
   const [showModal, setShowModal] = useState(false);
   const [selectedRental, setSelectedRental] = useState(null);
+
+  const [value, setValue] = useState({
+    startDate: null,
+    endDate: null,
+  });
+
+  const handleValueChange = (newValue) => {
+    console.log("newValue:", newValue);
+    setValue(newValue);
+  };
 
   const getRentals = async () => {
     const res = await fetchData("/dealer/cars/rentals", "GET", undefined);
@@ -93,12 +104,12 @@ const Home = () => {
                   htmlFor="customer_profile"
                   class="block mb-2 text-sm font-medium text-gray-900"
                 >
-                  Customer Profile
+                  Customer ID:
                 </label>
                 <input
                   type="text"
                   name="customer_profile"
-                  value={`${userCtx.first_name} ${userCtx.last_name}`}
+                  value={userCtx.nric}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5"
                   readOnly
                 />
@@ -133,47 +144,20 @@ const Home = () => {
                   readOnly
                 />
               </div>
-              <div date-rangepicker class="flex items-center">
-                <div class="relative">
-                  <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg
-                      class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                    </svg>
-                  </div>
-                  <input
-                    name="start"
-                    type="text"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Select date start"
-                  />
-                </div>
-                <span class="mx-4 text-gray-500">to</span>
-                <div class="relative">
-                  <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg
-                      class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                    </svg>
-                  </div>
-                  <input
-                    name="end"
-                    type="text"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Select date end"
-                  />
-                </div>
+              <div>
+                <Datepicker
+                  useRange={false}
+                  value={value}
+                  onChange={handleValueChange}
+                />
               </div>
+              <button
+                type="submit"
+                className="w-fit h-10 bg-primary text-white py-2 px-4 transition-all duration-200 rounded hover:bg-neutralGrey
+            rounded-lg "
+              >
+                Confirm
+              </button>
             </form>
           </div>
         </Modal>
--- rentorbuy_frontEnd/tailwind.config.js
+++ rentorbuy_frontEnd/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
     "node_modules/flowbite-react/**/*",
     "index.html",
     "./src/**/*.{html,js,jsx}",
