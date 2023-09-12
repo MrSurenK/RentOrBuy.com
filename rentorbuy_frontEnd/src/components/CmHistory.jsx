e925a03dc5db052db8b74a69aa70eb1ab002dbfb
K.Surendraraj <surenkworkz@gmail.com>
2023-09-13 01:53:11 +0800

    added component for transaction history

--- rentorbuy_frontEnd/src/components/CmHistory.jsx
+++ rentorbuy_frontEnd/src/components/CmHistory.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const CmHistory = () => {
-  return <div>Customer History</div>;
+  return (
+    <>
+      <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen  ">
+        <div className="w-full mx-auto"></div>
+      </div>
+    </>
+  );
 };
 
 export default CmHistory;
