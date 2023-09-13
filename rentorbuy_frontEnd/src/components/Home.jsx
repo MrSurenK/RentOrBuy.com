9e4413f108e387a022065d127c288e2970b349a5
K.Surendraraj <surenkworkz@gmail.com>
2023-09-13 14:42:59 +0800

    sale id done

--- rentorbuy_frontEnd/src/components/Home.jsx
+++ rentorbuy_frontEnd/src/components/Home.jsx
@@ -11,9 +11,11 @@ const Home = () => {
   const fetchData = useFetch();
   const userCtx = useContext(UserContext);
   const [rentals, setRentals] = useState([]);
+  const [sale, setSale] = useState([]);
 
   const [showModal, setShowModal] = useState(false);
   const [selectedRental, setSelectedRental] = useState(null);
+  const [selectedSale, setSelectedSale] = useState(null);
 
   // Custom date picker configuration
   const [value, setValue] = useState({
@@ -42,7 +44,21 @@ const Home = () => {
   useEffect(() => {
     console.log(userCtx);
     getRentals();
+    getSales();
   }, []);
+
+  // GET Sales API
+
+  const getSales = async () => {
+    const res = await fetchData("/dealer/cars/forsale", "GET", undefined);
+    if (res.ok) {
+      setSale(res.data);
+      console.log(res.data);
+    } else {
+      alert(JSON.stringify(res.data));
+      console.log(res.data);
+    }
+  };
 
   // POST CUSTOMER RENTALS API
   const handleRentalBooking = async (e) => {
@@ -189,6 +205,133 @@ const Home = () => {
                   type="number"
                   name="rental_price"
                   value={selectedRental.rental_rate}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5"
+                  readOnly
+                />
+              </div>
+              <div>
+                <Datepicker
+                  useRange={false}
+                  value={value}
+                  onChange={handleValueChange}
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-fit h-10 bg-primary text-white py-2 px-4 transition-all duration-200 rounded hover:bg-neutralGrey
+            rounded-lg "
+              >
+                Confirm
+              </button>
+            </form>
+          </div>
+        </Modal>
+      )}
+
+      <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen mt-auto ">
+        <div className="text-3xl font-semibold mt-5">
+          <p>Sale Cars</p>
+          <div className="grid grid-cols-4">
+            <hr className="w-full h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 bg-primary"></hr>
+          </div>
+        </div>
+        <div className="flex flex-column justify-between">
+          {sale.map((sale, index) => (
+            <Card
+              key={index}
+              imgAlt="Rental Car"
+              imgSrc={import.meta.env.VITE_SERVER + sale.vehicle_image}
+              className="w-full ml-8"
+            >
+              <div>
+                {sale.brand} {sale.model}
+              </div>
+              <div>{sale.colour}</div>
+              <div>COE expiry: {sale.coe_expiry}</div>
+              <div>Sale Price: ${sale.sale_price}</div>
+              <div>Vehicle Type: {sale.vehicle_type}</div>
+              <div>Listing Date: {sale.listing_date}</div>
+              <button
+                className="bg-primary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-neutralGrey"
+                onClick={() => {
+                  if (userCtx.accessToken) {
+                    setShowModal(true);
+                    setSelectedSale(sale);
+                  } else {
+                    alert("Please sign in or sign up");
+                  }
+                }}
+              >
+                Book Now
+              </button>
+            </Card>
+          ))}
+        </div>
+      </div>
+
+      {selectedSale && userCtx.accessToken && (
+        <Modal isVisible={showModal} onClose={() => setShowModal(false)}>
+          <div className="py-6 px-6 lg:px-8 text-left">
+            <h3 class="mb-4 text-xl font-medium text-gray-900">
+              Confirm Rental
+            </h3>
+            <form className="space-y-6" action="#">
+              <div>
+                <label
+                  htmlFor="customer_profile"
+                  class="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Customer ID:
+                </label>
+                <input
+                  type="text"
+                  name="customer_profile"
+                  value={userCtx.nric}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5"
+                  readOnly
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="car_model"
+                  class="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Car Model
+                </label>
+                <input
+                  type="text"
+                  name="car_model"
+                  value={`${selectedSale.brand} ${selectedSale.model}`}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5"
+                  readOnly
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="car_id"
+                  class="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Car Id
+                </label>
+                <input
+                  type="text"
+                  name="car_model"
+                  value={selectedSale.vehicle_id}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5"
+                  readOnly
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="sale_price"
+                  class="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Sale Price
+                </label>
+                <input
+                  type="number"
+                  name="sale_price"
+                  value={selectedSale.sale_price}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5"
                   readOnly
                 />
