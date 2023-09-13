fdc6f254ec5de486186187eb79522649e185279d
K.Surendraraj <surenkworkz@gmail.com>
2023-09-13 22:40:12 +0800

    maxlength implemented in edit Cm

--- rentorbuy_frontEnd/src/components/CmAccount.jsx
+++ rentorbuy_frontEnd/src/components/CmAccount.jsx
@@ -229,6 +229,7 @@ const CmAccount = () => {
                 name="postal"
                 value={postal}
                 id="postal"
+                maxlength="6"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 onChange={handleInputChange}
               />
@@ -248,6 +249,7 @@ const CmAccount = () => {
                 id="contact"
                 value={contact}
                 pattern="^\+65 \d{8}$"
+                maxlength="12"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 onChange={handleInputChange}
               />
